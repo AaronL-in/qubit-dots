4c19698e03937715fca006bd4cef0524a19d0f75
madisonXschuetze <madi@schuetze.ca>
2022-02-01 09:26:47 -0500

    changed rotation function to account for a rotation around any axis specified by a 3-Dimensional vector

--- qudipy/control/voltage_pulses.py
+++ qudipy/control/voltage_pulses.py
@@ -95,29 +95,45 @@ def rot(axis, theta, n_qubits, active_qubits,
     # the ControlPulse.ctrl_pulses object
 
     '''
+    # make sure that the rotation_axis has 3 components
+    if len(rotation_axis) != 3:
+        print("Please enter an iterable with 3 components")
+        
+    #create variables for Vector Components to speed up run-time
+    n_x = rotation_axis[0]
+    n_y = rotation_axis[1]
+    n_z = rotation_axis[2]
+    
+    # make sure that the rotation_axis is a Bloch Vector, if not, make it unitary
+    norm = np.sqrt(n_x**2 + n_y**2 + n_z**2)
+    if norm != 1:
+        n_x = n_x/norm
+        n_y = n_y/norm
+        n_z = n_z/norm
+        
     # build a tuple of positions of inactive qubits
     all_qubits = range(1, n_qubits + 1)
     idle_qubits = tuple(set(all_qubits) - set(active_qubits))
-
+    
     # determining the constant voltage offset in case the actual voltage 
     # pulse is shifted anywhere in time
     v_offset = v_unit_shape(np.inf) # surely equal to offset at infinity
     delta_g_0 = delta_g_interp(v_offset)
-
+    
     # creating a control pulse object 
-
+    
     rot_pulse = ControlPulse(pulse_name='ROT{}_{}'.format(axis.upper(), theta), 
                                 pulse_type='effective')
-
+    
     # setting all delta_g values to offset values for all qubits
     # this will be updated for some qubits later on
-
+    
     for i in all_qubits:
-        rot_pulse.add_control_variable(var_name=f'delta_g_{i}', 
+        rot_pulse.add_control_variable(var_name= 'delta_g_{i}', 
                                     var_pulse=np.full(num_val, delta_g_0))
-        rot_pulse.add_control_variable(var_name=f'V_{i}', 
+        rot_pulse.add_control_variable(var_name= 'V_{i}', 
                                     var_pulse=np.full(num_val, v_offset))
-
+        
     # values of normalized time and pulse
     tau = np.linspace(0, 1, num_val)
     v_pulse = v_unit_shape(tau)
@@ -127,67 +143,56 @@ def rot(axis, theta, n_qubits, active_qubits,
     theta_rad = np.deg2rad(theta) % (2 * np.pi)
     if theta_rad > np.pi:
         theta_rad -= 2 * np.pi
-
+    # define alpha = theta / 2pi
+    alpha = theta_rad / (2 * np.pi)
+    
     # evaluating the integral of delta g pulse
     delta_g_pulse = delta_g_interp(v_unit_shape(tau))
     delta_g_int = np.trapz(delta_g_pulse, tau) - delta_g_0
-
+    
     # finding the pulse length and setting the correct time values for pulses
     T = 1.
-
-    if axis.upper()=='Z':
-        # in this case, only the voltage on active qubits changes
-        for i in active_qubits:
-            rot_pulse.ctrl_pulses[f'delta_g_{i}'] = delta_g_pulse
-            rot_pulse.ctrl_pulses[f'V_{i}'] = v_pulse
-
-        # pulse duration
-        T = 2 * theta_rad / (omega * delta_g_int)
-
-    if axis.upper() in ('X', 'Y'):
-        # determine the phase values during the pulse first
-        phis = np.full(num_val, 0.)
-        if axis.upper() == 'Y':
-            phis = np.full(num_val, 90)
-        if theta < 0:
-            phis = phis + 180
+    
+    #determine the phase values during the pulse -> list of phi's
+    phis = np.zeros(num_val)
+    for i in range(0,num_val):
+        t = tau[i]
+        if v_unit_shape(t) > 0:
+            phi = np.atan2(n_y, n_x)
+        else:
+            phi = np.atan2(n_y * -1, n_x * -1)
+        phis[i] = phi
         
-        rot_pulse.add_control_variable("phi", phis)
-        # Making the ESR pulse of the same shape as the g-factor pulse
-        # shifted by delta_g_0
-        # finding the coefficient first
-
-        A = 1 / np.sqrt( (2 * np.pi / theta_rad) ** 2 - 1)
-
-        # values of ESR field
-        omega_capital = A * omega * np.abs(delta_g_pulse - delta_g_0) / 2
-        b_rf = m_e / e * omega_capital 
-
-        rot_pulse.add_control_variable("B_rf", b_rf)
-
+    rot_pulse.add_control_variable("phi", phis)
+    # add phi's to the control variable
+    
+    #Calculate time of pulse for the rotation
+    factor = ((4 * np.pi)/omega)
+    if n_z == 0:
+        T = factor * ((np.sqrt(1 - (alpha**2 * (n_x**2 + n_y**2)))) / abs(delta_g_int))
+    else:
+        T = factor * ((alpha * abs(n_z)) / abs(delta_g_int))
+        
+    #change voltage on qubits depending on the rotation
+    ## *******************
+    ## TODO ask Bohdan how voltages and deviation g-factors are changed when n_z =! 1, n_x != 1, and n_y != 1 
+        
+    if n_z == 1: #z-rotation
+        for i in active_qubits:
+            rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = delta_g_pulse
+            rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
+    else: #not a pure z-rotation
         # unlike the case of ROTZ, the voltage here changes only on the 
         # idling qubits so that they return to their initial state
         # after the pulse
         for i in idle_qubits:
-            rot_pulse.ctrl_pulses[f'delta_g_{i}'] = delta_g_pulse
-            rot_pulse.ctrl_pulses[f'V_{i}'] = v_pulse
-
-        # pulse duration
-        alpha = theta_rad / (2 * np.pi)
-        T = 4 * np.pi / (omega * delta_g_int) *  np.sqrt(1 - alpha ** 2)
-        
-
+            rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = delta_g_pulse
+            rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
+            
     # finally, specifying the correct time values for the pulse and returning it
     rot_pulse.add_control_variable('time', tau * T)
-
-    return rot_pulse
     
+    return rot_pulse
 
-
+    
     #TODO pandas recall how to add values
-
-
-
-
-
-
