fixed small errors, rotation axis can be a string or 3-D vector, created function to determine delta g-factor for active and inactive qubits

qudipy/control/voltage_pulses.py
@@ -44,7 +44,6 @@ def balance_zeeman(delta_g_interp, v_offset, f_rf):
     return B_0
 
 
-
 def rot(rotation_axis, theta, n_qubits, active_qubits, 
                 delta_g_interp, v_unit_shape, B_0, num_val=100):
     '''
@@ -55,7 +54,7 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
 
     Parameters
     -----------------
-    axis: string
+    rotation_axis: iterable of length 3 OR string ('X', 'Y', 'Z') 
         Axis of qubit rotation.
     theta: float
         Angle of qubit rotation in **degrees**.
@@ -95,22 +94,12 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
     # the ControlPulse.ctrl_pulses object
 
     '''
-    # make sure that the rotation_axis has 3 components
-    if len(rotation_axis) != 3:
-        print("Please enter an iterable with 3 components")
-        
-    #create variables for Vector Components to speed up run-time
-    n_x = rotation_axis[0]
-    n_y = rotation_axis[1]
-    n_z = rotation_axis[2]
+    # make sure that the rotation_axis has 3 components or is an appropriate axis
+    if rotation_axis in ['X', 'x', 'Y', 'y', 'Z', 'z']:
+        axis = rotation_axis.upper()
+    elif len(rotation_axis) != 3:
+        print("Please enter an iterable with 3 Float components or enter one of 'X', 'Y', 'Z'")
     
-    # make sure that the rotation_axis is a Bloch Vector, if not, make it unitary
-    norm = np.sqrt(n_x**2 + n_y**2 + n_z**2)
-    if norm != 1:
-        n_x = n_x/norm
-        n_y = n_y/norm
-        n_z = n_z/norm
-        
     # build a tuple of positions of inactive qubits
     all_qubits = range(1, n_qubits + 1)
     idle_qubits = tuple(set(all_qubits) - set(active_qubits))
@@ -121,19 +110,25 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
     delta_g_0 = delta_g_interp(v_offset)
     
     # creating a control pulse object 
-    
-    rot_pulse = ControlPulse(pulse_name='ROT{}_{}'.format(axis.upper(), theta), 
+    if isinstance(axis, str):
+        rot_pulse = ControlPulse(pulse_name='ROT{}_{}'.format(axis, theta), 
+                                pulse_type='effective')
+    else:
+        n_x = rotation_axis[0]
+        n_y = rotation_axis[1]
+        n_z = rotation_axis[2]
+        rot_pulse = ControlPulse(pulse_name='ROT({},{},{})_{}'.format(n_x, n_y, n_z, theta),
                                 pulse_type='effective')
     
     # setting all delta_g values to offset values for all qubits
     # this will be updated for some qubits later on
     
     for i in all_qubits:
-        rot_pulse.add_control_variable(var_name= 'delta_g_{i}', 
+        rot_pulse.add_control_variable(var_name= 'delta_g_{ind}'.format(ind = i), 
                                     var_pulse=np.full(num_val, delta_g_0))
         rot_pulse.add_control_variable(var_name= 'V_{i}', 
                                     var_pulse=np.full(num_val, v_offset))
-        
+
     # values of normalized time and pulse
     tau = np.linspace(0, 1, num_val)
     v_pulse = v_unit_shape(tau)
@@ -153,35 +148,36 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
     # finding the pulse length and setting the correct time values for pulses
     T = 1.
     
-    #determine the phase values during the pulse -> list of phi's
-    phis = np.zeros(num_val)
-    for i in range(0,num_val):
-        t = tau[i]
-        if v_unit_shape(t) > 0:
-            phi = np.atan2(n_y, n_x)
-        else:
-            phi = np.atan2(n_y * -1, n_x * -1)
-        phis[i] = phi
-        
-    rot_pulse.add_control_variable("phi", phis)
-    # add phi's to the control variable
-    
-    #Calculate time of pulse for the rotation
-    factor = ((4 * np.pi)/omega)
-    if n_z == 0:
-        T = factor * ((np.sqrt(1 - (alpha**2 * (n_x**2 + n_y**2)))) / abs(delta_g_int))
-    else:
-        T = factor * ((alpha * abs(n_z)) / abs(delta_g_int))
-        
-    #change voltage on qubits depending on the rotation
-    ## *******************
-    ## TODO ask Bohdan how voltages and deviation g-factors are changed when n_z =! 1, n_x != 1, and n_y != 1 
-        
-    if n_z == 1: #z-rotation
+    if axis == 'Z':
+        # in this case, only the voltage on active qubits changes
         for i in active_qubits:
             rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = delta_g_pulse
             rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
-    else: #not a pure z-rotation
+            
+        # pulse duration
+        T = 2 * theta_rad / (omega * delta_g_int)
+        
+    elif axis in ['X', 'Y']:
+        # determine the phase values during the pulse first
+        phis = np.full(num_val, 0.)
+        if axis.upper() == 'Y':
+            phis = np.full(num_val, 90)
+        if theta < 0:
+            phis = phis + 180
+            
+        rot_pulse.add_control_variable("phi", phis)
+        # Making the ESR pulse of the same shape as the g-factor pulse
+        # shifted by delta_g_0
+        # finding the coefficient first
+        
+        A = 1 / np.sqrt( (2 * np.pi / theta_rad) ** 2 - 1)
+        
+        # values of ESR field
+        omega_capital = A * omega * np.abs(delta_g_pulse - delta_g_0) / 2
+        b_rf = m_e / e * omega_capital 
+        
+        rot_pulse.add_control_variable("B_rf", b_rf)
+        
         # unlike the case of ROTZ, the voltage here changes only on the 
         # idling qubits so that they return to their initial state
         # after the pulse
@@ -189,6 +185,97 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
             rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = delta_g_pulse
             rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
             
+        # pulse duration
+        alpha = theta_rad / (2 * np.pi)
+        T = 4 * np.pi / (omega * delta_g_int) *  np.sqrt(1 - alpha ** 2)
+    
+    else:
+        # *** ARBITRARY ROTATION ***
+        #create variables for Vector Components to speed up run-time
+        n_x = rotation_axis[0]
+        n_y = rotation_axis[1]
+        n_z = rotation_axis[2]
+        
+        # make sure that the rotation_axis is a Bloch Vector, if not, make it unitary
+        norm = np.sqrt(n_x**2 + n_y**2 + n_z**2)
+        if norm != 1:
+            n_x = n_x/norm
+            n_y = n_y/norm
+            n_z = n_z/norm
+            
+        axis = [n_x, n_y, n_z]
+        
+        def delta_g_arbitraryROT(unit_rotation_axis, active, theta, v_unit_shape):
+            '''
+            Calculates delta_g as a function of the shape function for resonant (active) and
+            non-resonant (inactive) qubits
+            
+            Parameters:
+            -------------
+            unit_rotation_axis: iterable of length 3
+                The Bloch vector axis of rotation
+            active: Boolean 
+                if True, the function will return delta_g for resonant qubits, otherwise, it returns delta_g for 
+                non-resonant qubits
+            theta: Float
+                angle of rotation in radians
+            v_unit_shape: function
+                function describing the unitary voltage shape of the pulse
+
+            Returns:
+            -------------
+            iterable of length num_val 
+            '''
+            omega = e / m_e * B_0 #Larmor Frequency
+            tau = np.linspace(0,1,num_val)
+            n_x = unit_rotation_axis[0]
+            n_y = unit_rotation_axis[1]
+            n_z = unit_rotation_axis[2]
+            
+            v_shape_vals = []
+            for t in tau:
+                v_shape_vals.append(v_unit_shape(t))
+            if active:
+                return v_shape_vals * (2 * n_z * theta)/omega
+            else:
+                return v_shape_vals * (2/omega) * np.sqrt(4 * np.pi**2 - theta**2 * (n_x**2 + n_y**2))
+
+        #Update the Control Pulse Object
+        d_g_active = delta_g_arbitraryROT([n_x, n_y, n_z], True, theta, v_unit_shape)
+        d_g_inactive =delta_g_arbitraryROT([n_x, n_y, n_z], False, theta, v_unit_shape)
+
+        for i in active_qubits: 
+            rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = d_g_active
+            rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
+
+        for i in idle_qubits:
+            rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = d_g_inactive
+            rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
+
+
+        #determine the phase values during the pulse -> list of phi's
+        phis = np.zeros(num_val)
+        for i in range(0,num_val):
+            t = tau[i]
+            if v_unit_shape(t) > 0:
+                phi = np.atan2(n_y, n_x)
+            else:
+                phi = np.atan2(n_y * -1, n_x * -1)
+            phis[i] = phi
+        
+        rot_pulse.add_control_variable("phi", phis)
+        # add phi's to the control variable
+    
+        #Calculate time of pulse for the rotation
+        factor = ((4 * np.pi)/omega)
+        if n_z == 0:
+            T = factor * ((np.sqrt(1 - (alpha**2 * (n_x**2 + n_y**2)))) / abs(delta_g_int))
+        else:
+            T = factor * ((alpha * abs(n_z)) / abs(delta_g_int))
+        
+
+
+
     # finally, specifying the correct time values for the pulse and returning it
     rot_pulse.add_control_variable('time', tau * T)
     
@@ -196,3 +283,6 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
 
     
     #TODO pandas recall how to add values
+    
+    #TODO test out rotation code by seeing if [1,0,0] == 'X' rotation and so on - if so, use the simpler 'X'/'Y' rotation for the vector as well
+    