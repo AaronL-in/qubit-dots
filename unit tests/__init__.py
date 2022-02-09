fcbc3284a0c7f9364aba9fb40be619b4c4f60f30
zmerino <zmerino2718@gmail.com>
2022-02-08 23:45:10 -0500

    unit test updates

--- unit tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Created on Thu Jan  6 12:43:00 2022
-
-@author: Zach
-"""
-
-from . import qutils_test
--- unit tests/qutils_test/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Created on Thu Feb 7 21:43:00 2022
-
-@author: Zach
-"""
-
-# import any sub modules
