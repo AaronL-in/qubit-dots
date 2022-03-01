c0e6b0c0f172ba2a371b0c303c899da033cff85e
madisonXschuetze <madi@schuetze.ca>
2022-03-01 09:12:26 -0500

    added function 'create_ctlp_file' that takes a ControlPulse object and creates a .ctrlp file from it. Also added function 'create_quantum_circuit' that creates a QuantumCircuit object from a list of .ctrlp files, and if parameters are specified as True, writes a .qcric file and prints the directory

--- qudipy/control/voltage_pulses.py
+++ qudipy/control/voltage_pulses.py
@@ -3,6 +3,12 @@
     Will return control pulse objects eventually, but now it is used only to 
     find pulse duration or amplitude
 '''
+##*************************************************
+import os, sys
+sys.path.append(os.path.dirname(os.getcwd()))
+## ************************************************
+
+
 
 #TODO:
 # search function for delta g = g_0 for all qubits
@@ -12,6 +18,8 @@ import numpy as np
 import pandas as pd
 # TODO UNCOMMENT from qudipy.control import shapes
 from qudipy.circuit.control_pulse import ControlPulse
+from qudipy.circuit.quantum_circuit import QuantumCircuit
+from qudipy.circuit.file_parsers import load_pulses, _load_one_pulse, load_circuit
 from scipy.constants import e, m_e
 
 def balance_zeeman(delta_g_interp, v_offset, f_rf):
@@ -24,7 +32,7 @@ def balance_zeeman(delta_g_interp, v_offset, f_rf):
         (ideally, but only one interpolating function for now)
         Iterable of interpolating functions \delta g_i(\vec{V}) for 
         all qubits, including inactive ones
-    v_offset: function
+    v_offset: Float
         Voltage offset when no pulse is applied.
     f_rf: float
         ESR frequency.
@@ -40,8 +48,7 @@ def balance_zeeman(delta_g_interp, v_offset, f_rf):
         The value of Zeeman field that ensures 
         no rotation occurs on any of the idling qubits
     '''
-
-    delta_g_0 = delta_g_interp(v_offset(np.inf))
+    delta_g_0 = delta_g_interp(v_offset)
 
     
     omega = 2 * np.pi * f_rf / (1 + delta_g_0 / 2)
@@ -53,6 +60,8 @@ def balance_zeeman(delta_g_interp, v_offset, f_rf):
 def rot(rotation_axis, theta, n_qubits, active_qubits, 
                 delta_g_interp, v_unit_shape, B_0, num_val=100):
     '''
+    @author: Madi & Others (TODO specify others)
+
     Chooses the optimal duration for a ROT(\theta) pulse of the specified pulse 
     shape, where the argument 'axis' defines rotation. 
     ***For now*** the function assumes for the sake of simplicity that
@@ -136,13 +145,16 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
                                 pulse_type='effective')
     
     # setting all delta_g values to offset values for all qubits
+    # setting all Voltage values of plunger gates
     # this will be updated for some qubits later on
     
     for i in all_qubits:
         rot_pulse.add_control_variable(var_name= 'delta_g_{ind}'.format(ind = i), 
                                     var_pulse=np.full(num_val, delta_g_0))
-        rot_pulse.add_control_variable(var_name= 'V_{i}', 
+        rot_pulse.add_control_variable(var_name= 'V_{ind}'.format(ind = i), 
                                     var_pulse=np.full(num_val, v_offset))
+    # adding in active qubits variable
+    setattr(rot_pulse, 'active', active_qubits) #TODO figure out why .set_active isn't working in ControlPulse Class
 
     # values of normalized time and pulse
     tau = np.linspace(0, 1, num_val)
@@ -222,16 +234,16 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
         
         def delta_g_arbitraryROT(unit_rotation_axis, active, theta, v_unit_shape):
             '''
-            Calculates delta_g as a function of the shape function for resonant (active) and
-            non-resonant (inactive) qubits
+            Calculates delta_g as a function of the shape function for resonant 
+            (active) and non-resonant (inactive) qubits
             
             Parameters:
             -------------
             unit_rotation_axis: iterable of length 3
                 The Bloch vector axis of rotation
             active: Boolean 
-                if True, the function will return delta_g for resonant qubits, otherwise, it returns delta_g for 
-                non-resonant qubits
+                if True, the function will return delta_g for resonant qubits, 
+                otherwise, it returns delta_g for non-resonant qubits
             theta: Float
                 angle of rotation in radians
             v_unit_shape: function
@@ -255,11 +267,36 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
                     delta_g.append(v * parameter)
                 return delta_g
             else:
-                parameter = (2/omega) * np.sqrt(4 * np.pi**2 - theta**2 * (n_x**2 + n_y**2))
+                parameter = (2/omega) * np.sqrt(4 * np.pi**2 - theta**2 * 
+                                                            (n_x**2 + n_y**2))
                 for v in v_shape_vals:
                     delta_g.append(v * parameter)
                 return delta_g
-
+        
+        
+        #determine the phase values during the pulse -> list of phi's
+        phis = np.zeros(num_val)
+        for i in range(0,num_val):
+            t = tau[i]
+            if v_unit_shape(t) > 0:
+                phi = np.arctan2(n_y, n_x)
+            else:
+                phi = np.arctan2(n_y * -1, n_x * -1)
+            phis[i] = phi
+            
+        rot_pulse.add_control_variable("phi", phis)
+        # add phi's to the control variable
+        
+        
+        #Calculate time of pulse for the rotation
+        factor = ((4 * np.pi)/omega)
+        if n_z == 0:
+            T = factor * ((np.sqrt(1 - (alpha**2 * (n_x**2 + n_y**2)))) /
+                                                            abs(delta_g_int))
+        else:
+            T = factor * ((alpha * abs(n_z)) / abs(delta_g_int))
+        
+        
         #Add ESR frequency into the Control Pulse
         v_shape_vals = v_unit_shape(tau)
         v_shape_sign = []
@@ -273,15 +310,15 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
         B_rf =[]
         parameter = abs(theta_rad) * np.sqrt(n_x**2 + n_y**2) #saves calculation time
         for i in range(len(tau)):
-            omega_capital_tau = v_shape_vals[i] * parameter * v_shape_sign[i]
+            omega_capital_tau = (v_shape_vals[i] / T[i]) * parameter * v_shape_sign[i]
             omega_capital.append(omega_capital_tau)
             b_rf = m_e / e * omega_capital[i]
             B_rf.append(b_rf)
         rot_pulse.add_control_variable('B_rf', B_rf)
         
         #Update the Control Pulse Object
-        d_g_active = delta_g_arbitraryROT([n_x, n_y, n_z], True, theta_rad, v_unit_shape) 
-        d_g_inactive = delta_g_arbitraryROT([n_x, n_y, n_z], False, theta_rad, v_unit_shape) 
+        d_g_active = delta_g_arbitraryROT(axis, True, theta_rad, v_unit_shape) 
+        d_g_inactive = delta_g_arbitraryROT(axis, False, theta_rad, v_unit_shape) 
 
         for i in active_qubits: 
             rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = d_g_active
@@ -291,27 +328,7 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
             rot_pulse.ctrl_pulses['delta_g_{ind}'.format(ind = i)] = d_g_inactive
             rot_pulse.ctrl_pulses['V_{ind}'.format(ind = i)] = v_pulse
 
-
-        #determine the phase values during the pulse -> list of phi's
-        phis = np.zeros(num_val)
-        for i in range(0,num_val):
-            t = tau[i]
-            if v_unit_shape(t) > 0:
-                phi = np.arctan2(n_y, n_x)
-            else:
-                phi = np.arctan2(n_y * -1, n_x * -1)
-            phis[i] = phi
-        
-        rot_pulse.add_control_variable("phi", phis)
-        # add phi's to the control variable
     
-        #Calculate time of pulse for the rotation
-        factor = ((4 * np.pi)/omega)
-        if n_z == 0:
-            T = factor * ((np.sqrt(1 - (alpha**2 * (n_x**2 + n_y**2)))) / abs(delta_g_int))
-        else:
-            T = factor * ((alpha * abs(n_z)) / abs(delta_g_int))
-
     # finally, specifying the correct time values for the pulse and returning it
     rot_pulse.add_control_variable('time', tau * T)
     
@@ -320,5 +337,239 @@ def rot(rotation_axis, theta, n_qubits, active_qubits,
     
     #TODO pandas recall how to add values
     
-    #TODO test out rotation code by seeing if [1,0,0] == 'X' rotation and so on - if so, use the simpler 'X'/'Y' rotation for the vector as well
+
+def create_ctlp_file(CntrlPulse, read = False, get_dir = False):
+    '''
+    @author: Madi
+
+    Creates a .ctrlp file containing the information about a rotation gate
+    from a ControlPulse object
+
+    Parameters
+    -------------
+    CntrlPulse: ControlPulse
+        Control Pulse Object 
+    read: Bool
+        If read = True, the function will print out the .ctrlp file, 
+        if read = False (Default) the funciton will print nothing
+    get_dir: Bool
+        If get_dir = True, the function will return the directory as a 
+        string, if get_dir = False (Default), the function will return nothing
+
+    Returns
+    -------------
+    Str *conditional*
+        Returns the directory of the file as a str if get_dir = True, otherwise,
+        the function returns None
+
+    None
+
+    Effects
+    -------------
+    Creates a .ctrlp file containing all information to re-create the same ControlPulse 
+    object
+
+    Prints to screen *conditional* - prints the Control Pulse file to the screen if 
+    read = True, otherwise, nothing is printed to the screen
+    '''
+    # retrieve all pieces of information
+    name = CntrlPulse.name 
+    ideal_gate = CntrlPulse.ideal_gate
+    p_type = CntrlPulse.pulse_type
+    p_length = CntrlPulse.length
+    pulse_key = CntrlPulse.ctrl_names
+    pulse_vals = list(CntrlPulse.ctrl_pulses.values())
+    active_qubits = CntrlPulse.active
+    
+    # format Control Pulse data into writable strings
+    key_str = ""
+    vals_str = ""
+    for i, key in enumerate(pulse_key):
+        if i == len(pulse_key) -1:
+            key_str = key_str + str(key).strip()
+        else:
+            key_str = key_str + str(key).strip() + "," 
+    
+    for j in range(len(pulse_vals[0])):
+        vals_str_line = ""
+        for i in range(len(pulse_key)):
+            if i == (len(pulse_key) - 1):
+                vals_str_line = vals_str_line + str(pulse_vals[i][j])
+            else:
+                vals_str_line = vals_str_line + str(pulse_vals[i][j]) + ", "
+        if j == (len(pulse_vals[0]) - 1):
+            vals_str = vals_str + vals_str_line
+        else:
+            vals_str = vals_str + vals_str_line + "\n"
+    
+    actv_str = ""
+    if active_qubits != None:
+        for i, q in enumerate(active_qubits):
+            if i == (len(active_qubits) - 1):
+                actv_str = actv_str + str(q)
+            else:
+                actv_str = actv_str + str(q) + ", "
+    
+    # Write the .ctrlp file
+    ctrlp_file = open(name + '.ctrlp', 'w')
+    ctrlp_file.writelines(["# {n}.ctrlp\n".format(n = name),
+                           "Ideal gate: {ig}\n".format(ig = ideal_gate),
+                           "Pulse type: {pt}\n".format(pt = p_type),
+                           "Pulse length: {pl}\n".format(pl = p_length),
+                           "Active qubits: {aq}\n".format(aq = actv_str),
+                           "Control pulses:\n",
+                            key_str + "\n",
+                            vals_str])
+    
+    # close file to stop making any changes
+    ctrlp_file.close()
+    
+    # read the file if necessary
+    if read == True:
+        directory = os.getcwd() + "/" + name + ".ctrlp"
+        f = open(directory, 'r')
+        print(f.read())
+    
+    # return the directory if necessary
+    if get_dir == True:
+        directory = os.getcwd() + "/" + name + ".ctrlp"
+        return(directory)
+
+
+
+
+def create_create_quantum_circuit(list_ctrlp, circuit_name, write_qcirc = False, print_dir = False):
+    '''
+    @author: Madi
+
+    Takes directory to a .qcirc file, creates the necessary 
+    ControlPulses for rotations around an axis, creates a dictionary 
+    of these specified ControlPulse objects and returns a QuantumCircuit Object
+
+    Parameters
+    --------------
+    list_ctrlp: Listof(Str(.ctrlp))
+        A list that contains .ctrlp files where these files are specified by strings
+    circuit_name: str
+        The name that the user wishes to call the circuit
+    write_qcirc: Bool
+        If write_qcirc = True the function will write a qcirc file of the quantum circuit
+        with the name "circuit_name".qcirc, else the function does nothing
+    print_dir: Bool
+        If write_qcirc = True and print_dir = True the function will print the directory 
+        of the newly made .qcirc file, else the function does nothing
+
+    Requires
+    ----------
+    * All ControlPulse objects act on the same number of qubits 
+
+    * All .ctrlp files in list_ctrlp will path to the correct directory
+        -> user must ensure that all directory satisfactions are met 
+
+    Returns
+    ---------
+        QuantumCircuit object
+
+    Effects
+    ---------
+    Writes File *conditional*
+        writes a .qcirc file with the name "circuit_name".qcirc is write_qcirc = True
+    Prints to Screen
+        prints the directory of the newly made .qcirc file if print_dir = True and 
+        write_qcirc = True
+    '''
+    pulse_dict = load_pulses(list_ctrlp) 
+    
+    file = open(list_ctrlp[0], 'r')
+    L = file.readlines()
+    for i, line in enumerate(L):
+        if "control" in line.lower():
+            ind = i + 1
+            break
+    cnt_dict = {'V':0}
+    for s in L[ind]:
+        if s == 'V':
+            cnt_dict['V'] = cnt_dict['V'] + 1
+    n_qubits = cnt_dict['V']
+    
+    file.close()
+    
+    ## ** Write .qcirc file if necessary **
+    if write_qcirc == True:
+        Gate_Str = ""
+        
+        # Create List of active qubits and List of qubit names, 
+        # then make it into a writeable String
+        for i in range(len(list_ctrlp)):
+            f = open(list_ctrlp[i], 'r')
+            L = f.readlines()
+            actv_qubit_str = None
+            name_str = None
+            for param in L:
+                if actv_qubit_str != None and name_str != None:
+                    break
+                elif "#" in param.lower(): #elif statement to format name correctly
+                    name_str = param.split(".")[0].split("#")[1].strip()
+                    if "ROT" in name_str:
+                        name_str = name_str.replace("OT","")
+                    num_count = 0
+                    negative = ""
+                    for s in name_str[name_str.index("_") + 1:]:
+                        if s == "-":
+                            negative = "-"
+                        elif isinstance(int(s), int):
+                            num_count = num_count + 1
+                    if num_count == 1:
+                        name_str = name_str.replace("_","{neg}00").format(neg = negative)
+                    if num_count == 2:
+                        name_str = name_str.replace("_","{neg}0").format(neg = negative)
+                    else:
+                        name_str = name_str.replace("_","{neg}").format(neg = negative)
+                    
+                elif "active" in param.lower():
+                    actv_qubit_str = param.split(": ")[1].replace(",","")
+            if i == (len(list_ctrlp) - 1):
+                Gate_Str = Gate_Str + "{name} {active}".format(name = name_str, active = actv_qubit_str)
+            else:
+                Gate_Str = Gate_Str + "{name} {active}".format(name = name_str, active = actv_qubit_str)
+        
+        # create the new qcirc file
+        qcirc_file = open(circuit_name + ".qcirc", 'w')
+        qcirc_file.writelines(["Number of qubits: {nq}\n".format(nq = n_qubits), Gate_Str])
+        qcirc_file.close()
+        
+        if print_dir == True:
+            print(os.getcwd() + "/" + circuit_name + ".qcirc")
+
+    
+    ## ** Create Quantum Circuit Object with the newly made pulse_dict **
+    return QuantumCircuit(circuit_name, n_qubits, pulse_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
