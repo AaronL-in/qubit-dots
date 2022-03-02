0e664ac51640a636e1614ed16935ac6bdac7c158
madisonXschuetze <madi@schuetze.ca>
2022-03-02 13:48:31 -0500

    put test_starkshift in line with pytest package

--- unit_tests/test_starkshift.py
+++ unit_tests/test_starkshift.py
@@ -1,42 +1,42 @@
+## *****************************************
+##          Test Starkshift
+##          testing starkshift module
+## *****************************************
 
-if __name__=='__main__':
-    # for the case when the test is run from the "unit_tests" folder
-    import sys, os
-    up = os.path.abspath('..')
-    sys.path.insert(0, up)
+import os, sys
+sys.path.append(os.path.dirname(os.getcwd()))
+
 
 import numpy as np
 from random import random
+import pytest
 
 from qudipy.starkshift import starkshift
 
 
 
 
-class TestStarkshiftObject:
-    def test_temp(self):
-        '''
-        Verifies that the temperature parameter and change_temperature function
-        works.
-        '''
-        T = self.temp
-        self.change_temperature(T + 5)
-        np.testing.assert_equal(self.temp, T + 5, "Temperatures Not equal, Test Failed", verbose=True)
+def test_temp():
+    '''
+    Verifies that the temperature parameter and change_temperature function
+    works.
+    '''
+    T = self.temp
+    self.change_temperature(T + 5)
+    assert self.temp == T + 5, "Temperatures Not equal, Test Failed"
 
 
-    def test_temp_interp(self):
-        '''
-        Verifies that the correct interpolated g-factor value is returned 
-        when different temperatures are specified in the object.
-        '''
-        self.change_temperature(0)
-        delta_g = self.temp_g_factor('Si') #uses the temperature in the Class Object
-        np.testing.assert_approx_equal(delta_g, 1.99875, significant = 6)
+def test_temp_interp():
+    '''
+    Verifies that the correct interpolated g-factor value is returned 
+    when different temperatures are specified in the object.
+    '''
+    self.change_temperature(0)
+    delta_g = self.temp_g_factor('Si') #uses the temperature in the Class Object
+    assert delta_g == pytest.approx(1.99875, 1e-6)
 
-        self.change_temperature(100)
-        delta_g = self.temp_g_factor('Si') #uses the temperature in the Class Object
-        np.testing.assert_approx_equal(delta_g, 1.99865, significant = 6)
+    self.change_temperature(100)
+    delta_g = self.temp_g_factor('Si') #uses the temperature in the Class Object
+    assert delta_g == pytest.approx(1.99865, 1e-6)
 
-
-if __name__=='__main__':
-    os.system('python -m pytest')
+    
