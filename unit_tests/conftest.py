bc8507edf20c4f907a4f0c92f13bb1148a001956
madisonXschuetze <madi@schuetze.ca>
2022-03-21 12:05:13 -0400

    Revert to 9218eb233a5545ad44219361f0eff4480839436f

---
+++ unit_tests/conftest.py
@@ -0,0 +1,8 @@
+import os, sys
+sys.path.append(os.path.dirname(os.getcwd()))
+import pytest
+
+# import any packages used for testing the module
+import qudipy.qutils.matrices as matr
+
+# Add fixtures to be used accross all packages within a module
