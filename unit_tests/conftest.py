ba28ee286f48c73c5dec80d1f4971881d9e171d5
zmerino <zmerino2718@gmail.com>
2022-03-24 16:43:53 -0400

    updated control branch with changes made to master

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -3,10 +3,7 @@ sys.path.append(os.path.dirname(os.getcwd()))
 import pytest
 
 # import any packages used for testing the module
-<<<<<<< HEAD
 import qudipy.potential as pot
-=======
 import qudipy.qutils.matrices as matr
->>>>>>> b75bb325477f8238580245c56a8cc373ecb8d70b
 
 # Add fixtures to be used accross all packages within a module
