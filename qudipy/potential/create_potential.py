b0ca579841eeeee859849df2ec105e71b3e2dabf
madisonXschuetze <madi@schuetze.ca>
2022-04-09 11:59:24 -0400

    Fixed '.quartic_potential()' and ',square_potential()' methods

--- qudipy/potential/create_potential.py
+++ qudipy/potential/create_potential.py
@@ -9,7 +9,7 @@ from  qudipy.potential import grid_params
 import qudipy as qd 
 
 class Potential:
-	def __init__(self, omegas, dot_sep, material, hor_comp = 10000):
+	def __init__(self, omegas, dot_sep, material, hor_comp = 10000, bias = 0, limit = 1/4):
 		'''
 		Initialize Potential object
 
@@ -25,6 +25,20 @@ class Potential:
 			determines that constants that we use. Must
 			be consistent with Constants class in 
 			qudipy/utils/constants.py 
+
+		hor_comp: Int
+			The horizontal compression of the well for 
+			the quartic well.
+			Default = 10000
+
+		bias: Anyof(Int, Float)
+			The constant value that the potential takes 
+			for a square or triangular well.
+			Default = 0
+
+		limit: Float
+			A half the fraction of the dot seperation that the well 
+			will span
 		
 		Requires
 		------------
@@ -46,6 +60,8 @@ class Potential:
 		self.material = material
 		self.consts = qd.Constants(material)
 		self.hor_scale = hor_comp
+		self.bias = bias
+		self.limit = limit
 	
 	
 	def harmonic_potential(self, ctrl_vals, gparams):
@@ -72,17 +88,7 @@ class Potential:
 		sep = self.dot_sep
 		
 		# create offset list
-		if ctrl_num % 2 == 0:
-			mid = int(ctrl_num/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.delete(np.arange(lbound, ubound), mid)
-		
-		else:
-			mid = int((ctrl_num - 1)/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.arange(lbound, ubound)
+		offset = offset_list(ctrl_num)
 		
 		# Create list of wells
 		well_list = []
@@ -125,34 +131,24 @@ class Potential:
 		sep = self.dot_sep
 		scale = self.hor_scale
 		
-		# create offset list
-		if ctrl_num % 2 == 0:
-			mid = int(ctrl_num/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.delete(np.arange(lbound, ubound), mid)
-			
-		else:
-			mid = int((ctrl_num - 1)/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.arange(lbound, ubound)
+		# Create offset list
+		offset = offset_list(ctrl_num)
 			
 		# Create list of wells
 		well_list = []
 		for i, off in enumerate(offset):
 			v_i = ctrl_vals[i]
-			mu = v_i * self.consts.e
+			mu =  10 * v_i * self.consts.e
 			omega = self.omegas[i]
 			well = 1/2 * omega * self.consts.e *\
-					(np.power((scale * x_pot + off * sep), 4) + np.power(scale * y_pot,4)) - mu
+					(np.power((scale * (x_pot + off * sep)), 4) + np.power(scale * y_pot,4)) - mu
 			well_list = well_list + [well]
 			
 		min = well_list[0]
 		for w in well_list:
 			min = np.minimum(min, w)
 			
-		return min
+		return min 
 
 	
 	def square_potential(self, ctrl_vals, gparams):
@@ -168,6 +164,10 @@ class Potential:
 		gparams: GridParameters object
 			specifies the x and y coordinates
 
+		bias: Anyof(Int, Float)
+			The constant voltage value that the well 
+			normally takes when it is not on a dip.
+
 		Returns
 		-------------
 		Arrayof(Arrayof(Float)) 
@@ -181,64 +181,70 @@ class Potential:
 		x_pot = gparams.x_mesh
 		y_pot = gparams.y_mesh
 		dot_sep = self.dot_sep
-		lenx = round(len(x_pot)/6)
-		leny = round(len(y_pot)/6)
+		bias = self.bias
+		limit = self.limit
 		
-		# create offset list - even
-		if ctrl_num % 2 == 0:
-			mid = int(ctrl_num/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.delete(np.arange(lbound, ubound), mid)
+		# Create offset list
+		offset = offset_list(ctrl_num)
 		
-		#create offset list - odd
-		else:
-			mid = int((ctrl_num - 1)/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.arange(lbound, ubound)
-		
-		# Find Spacing Dictionary
-		spacing_dict = {}
-		for off in offset:
-			lbound = (off - 1/4) * dot_sep 
-			ubound = (off + 1/4) * dot_sep
-			spacing_dict[(lbound, ubound)] = None
-		
-		# Fill in spacing dict with position numbers
-		# of where the well is non-zero
-		x = x_pot[0] #sample x-slice
-		for s in list(spacing_dict.keys()):
-			ind_list = []
-			for i in range(len(x)):
-				if s[0] <= x[i] <= s[1]:
-					ind_list = ind_list + [i]
-			spacing_dict[s] = (min(ind_list), max(ind_list))
-				
-		# Create list of wells
-		well_list = []
-		x_pot_new = x_pot * 0 
-		y_pot_new = y_pot * 0
-		for ind in range(len(x_pot)):
-			for i, s_ind in enumerate(list(spacing_dict.values())):
-				v_i = ctrl_vals[i]
-				mu = v_i * self.consts.e
-				omega = self.omegas[i]
-				# create x and y values array
-				xval = x_pot[ind][0] * 1/2 * self.consts.me * omega**2 - v_i
-				yval = y_pot[ind][0] * 1/2 * self.consts.me * omega**2 - v_i
-				for j in range(s_ind[0], s_ind[1] + 1):
-					x_pot_new[ind][j] = xval
-					y_pot_new[ind][j] = yval
+		# Create Well Array
+		def square(mesh, offset, limit, bias, dot_sep):
+			'''
+			Creates an array of mesh objects representing a 1-D square well
 
-			well = x_pot_new + y_pot_new
-			well_list = well_list + [well]
-			
-		return_well = well_list[0]
-		for w in well_list:
+			Parameters
+			-------------
+			mesh: ndarray (mesh grid object)
+				The x or y mesh object that defines the positions
+			offset: Array
+				The array containing the offset positions of each well
+			limit: Float
+				The a half the fraction of the dot seperation that the well 
+				will span
+			bias: Anyof(Int, Float)
+				The initial voltage in Volts
+			dot_sep: Anyof(Int, Float)
+				The dot seperation between wells
+
+			Returns
+			-------------
+			Arrayof(mesh object):
+				Returns an array of mesh objects Arrayof(ndarray) 
+				of a square well.
+
+			Requires
+			-------------
+			* 2*limit < dot seperation
+			'''
+			omegas = self.omegas 
+			well = bias * np.ones(np.shape(mesh))
+			well_list = []
+			for i, off in enumerate(offset):
+				v_i = ctrl_vals[i]
+				omega = omegas[i]
+				amp = 1/2 * self.consts.me * omega**2 - v_i
+				mask =  ((mesh >= (off - limit)*dot_sep) & (mesh <= (off + limit)*dot_sep))
+				well[mask] = amp
+				well_list = well_list + [well]
+				
+			return np.array(well_list)
+
+		# Create x_well array and y_well array
+		x_wells = square(x_pot, offset, limit, bias, dot_sep)
+		y_wells = square(y_pot, [0], limit, bias, dot_sep)
+
+		# Add arrays and take the minimum
+		well_array = x_wells + y_wells
+		
+		return_well = well_array[0]
+		for w in well_array:
 			return_well = np.minimum(return_well, w)
 		
-		return return_well
+		mask = (return_well > -0.3)
+		return_well[mask] = bias
+		
+		# Return the final well
+		return return_well 
 	
 	
 	def triangle_potential(self, ctrl_vals, gparams):
@@ -263,67 +269,97 @@ class Potential:
 		x_pot = gparams.x_mesh
 		y_pot = gparams.y_mesh
 		dot_sep = self.dot_sep
-		lenx = round(len(x_pot)/6)
-		leny = round(len(y_pot)/6)
+		bias = self.bias
+		limit = self.limit
 		
-		# create offset list - even
-		if ctrl_num % 2 == 0:
-			mid = int(ctrl_num/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.delete(np.arange(lbound, ubound), mid)
+		# Create offset list
+		offset = offset_list(ctrl_num)
+		
+		# Create Well Array
+		def triangle(mesh, offset, limit, bias, dot_sep):
+			'''
+			Creates an array of mesh objects representing a 1-D square well
+
+			Parameters
+			-------------
+			mesh: ndarray (mesh grid object)
+				The x or y mesh object that defines the positions
+			offset: Array
+				The array containing the offset positions of each well
+			limit: Float
+				The a half the fraction of the dot seperation that the well 
+				will span
+			bias: Anyof(Int, Float)
+				The initial voltage in Volts
+			dot_sep: Anyof(Int, Float)
+				The dot seperation between wells
+
+			Returns
+			-------------
+			Arrayof(mesh object):
+				Returns an array of mesh objects Arrayof(ndarray) 
+				of a square well.
+
+			Requires
+			-------------
+			* 2*limit < dot seperation
+			'''
+			omegas = self.omegas 
+			well = bias * np.ones(np.shape(mesh))
+			well_list = []
 			
-		#create offset list - odd
-		else:
-			mid = int((ctrl_num - 1)/2)
-			lbound = -1 * mid
-			ubound = mid + 1
-			offset = np.arange(lbound, ubound)
-			
-		# Find Spacing Dictionary
-		spacing_dict = {}
-		for off in offset:
-			lbound = (off - 1/4) * dot_sep 
-			ubound = (off + 1/4) * dot_sep
-			spacing_dict[(lbound, ubound)] = None
-			
-		# Fill in spacing dict with position numbers
-		# of where the well is non-zero
-		x = x_pot[0] #sample x-slice
-		for s in list(spacing_dict.keys()):
-			ind_list = []
-			for i in range(len(x)):
-				if s[0] <= x[i] <= s[1]:
-					ind_list = ind_list + [i]
-			spacing_dict[s] = (min(ind_list), max(ind_list))
-			
-		# Create list of wells
-		well_list = []
-		x_pot_new = x_pot * 0 
-		y_pot_new = y_pot * 0
-		for ind in range(len(x_pot)):
-			for i, s_ind in enumerate(list(spacing_dict.values())):
+			for i, off in enumerate(offset):
 				v_i = ctrl_vals[i]
-				mu = v_i * self.consts.e
-				omega = self.omegas[i]
-				# create x and y values array
+				mask =  ((mesh >= (off - limit)*dot_sep) & (mesh <= (off + limit)*dot_sep))
+				# Save the potential values to use each time
 				if i == 0:
-					range_vals = []
-					for j in range(s_ind[0], s_ind[1] + 1):
-						x_pot_new[ind][j] = x_pot[ind][j] * self.consts.me * v_i * omega**2 - mu
-						y_pot_new[ind][j] = y_pot[ind][j] * self.consts.me * v_i * omega**2 - mu
-						range_vals = range_vals + [j]
-				else:
-					for k, j in enumerate(range(s_ind[0], s_ind[1] + 1)):
-						r = range_vals[k]
-						x_pot_new[ind][j] = x_pot_new[ind][r]
-						y_pot_new[ind][j] = y_pot_new[ind][r]
-				
-			well = x_pot_new + y_pot_new
-			well_list = well_list + [well]
-			
-		return_well = well_list[0]
-		for w in well_list:
+					save_vals = mesh[mask]
+				well[mask] = save_vals * v_i
+				well_list = well_list + [well]
+			return np.array(well_list)
+		
+		# Create x_well array and y_well array
+		x_wells = triangle(x_pot, offset, limit, bias, dot_sep)
+		y_wells = triangle(y_pot, [0], limit, bias, dot_sep)
+		
+		# Add arrays and take the minimum
+		well_array = x_wells + y_wells
+		
+		return_well = well_array[0]
+		for w in well_array:
 			return_well = np.minimum(return_well, w)
-			
+		
+		# Return the final well
 		return return_well
+
+
+def offset_list(n_wells):
+	'''
+	Creates offset list for Potential Class
+	functions
+
+	Parameters
+	-------------
+	ctrl_num: Int
+		The number of wells
+
+	Returns
+	-------------
+	Returns the integer offset list used
+	in the Potential Class functions
+	'''
+	# create offset list - even
+	if n_wells % 2 == 0:
+		mid = int(n_wells/2)
+		lbound = -1 * mid
+		ubound = mid + 1
+		offset = np.delete(np.arange(lbound, ubound), mid)
+		
+	#create offset list - odd
+	else:
+		mid = int((n_wells - 1)/2)
+		lbound = -1 * mid
+		ubound = mid + 1
+		offset = np.arange(lbound, ubound)
+	
+	return offset
