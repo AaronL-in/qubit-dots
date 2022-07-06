296e7a7671fbcf3c9f5c51f9ac5be4b0c2ecfbf8
anaidaaali <107521215+anaidaaali@users.noreply.github.com>
2022-07-05 21:54:36 -0400

    Added load, save and process
    added functions for loading nextnano files, save them as json files and to read saved files in io folder

---
+++ io/__init__.py
@@ -0,0 +1,9 @@
+'''
+Code for handling processing of potential files (generated from nextnano++
+software)
+'''
+
+from .grid_params import GridParameters
+from .load_data import *
+from .potential_interpolator import PotentialInterpolator
+from .process_nextnano import *
